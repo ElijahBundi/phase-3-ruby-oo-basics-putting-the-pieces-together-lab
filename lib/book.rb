8a30230c1a281004b8fcf6f1fcbd729fecbdaea5
Elijah <bundielly@gmail.com>
2022-08-18 11:47:13 +0300

    commit tree a3e73e0dea3bac4d313e5d2d1ad267057326d77d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts 'Flipping the page...wow, you read fast!'
+    end
+end
+
+book =Book.new('Damu Nyeusi')
+puts book.title
+book.author = 'swahili mse'
+puts book.author 
+book.page_count = '292'
+puts book.page_count
+book.genre = 'setbook'
+puts book.genre
+
+puts book.turn_page
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        puts 'Your shoe is as good as new!'
+        @condition = 'new'
+    end
+end
+
+shoe = Shoe.new('adidas')
+puts shoe.brand
+shoe.color = 'blue'
+puts shoe.color 
+shoe.size = '29'
+puts shoe.size
+shoe.material = 'leather'
+puts shoe.material
+shoe.condition = 'new'
+puts shoe.condition
